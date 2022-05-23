a65a3a79bf61d33ae398e4eb87faaad01740f0b2
Yakimovich Sergey <yakimovichsergei@gmail.com>
2022-05-23 16:04:16 +0300

    create an object

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-console.log('Hello world!');
+"use strict";
+
+const numbersOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+
+const personalMovieDB = {
+    count: numbersOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    private: false
+};
+
+const a = prompt('Один из последних просмотренных фильмов?', ''),
+      b = prompt('На сколько оцените его?', ''),
+      c = prompt('Один из последних просмотренных фильмов?', ''),
+      d = prompt('На сколько оцените его?', '');  
+
+      personalMovieDB.movies[a] = b;
+      personalMovieDB.movies[c] = d;
+
+      console.log(personalMovieDB);
